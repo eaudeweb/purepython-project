f3f428480a8e61bf22532503680e718fd5f0d286
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-11-12 12:10:50 +0200

    Write the first view - news feed.

--- fb/views.py
+++ fb/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from fb.models import UserPost
+
+
+def index(request):
+    if request.method == 'GET':
+        posts = UserPost.objects.all()
+        context = {
+            'posts': posts,
+        }
+        return render(request, 'index.html', context)
