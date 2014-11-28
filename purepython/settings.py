702fb1e4c9dd5441131290ef9d52425bb292ba9f
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-11-28 14:09:04 +0200

    Add settings to accept user uploads.

--- purepython/settings.py
+++ purepython/settings.py
@@ -49,6 +49,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.media',
+)
+
 ROOT_URLCONF = 'purepython.urls'
 
 WSGI_APPLICATION = 'purepython.wsgi.application'
@@ -82,3 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- purepython/urls.py
+++ purepython/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from fb.views import index, post_details, login_view, logout_view
 
@@ -12,4 +14,4 @@ urlpatterns = patterns(
     url(r'^accounts/login/$', login_view, name='login'),
     url(r'^accounts/logout/$', logout_view, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
